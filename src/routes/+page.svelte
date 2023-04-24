dbe901ba43d5ea76473936197db6f42bf6982eec
demioo <demiogun1@gmail.com>
2023-04-24 11:18:20 +0100

    Basic tailwind setup

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,48 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import '../app.css';
+</script>
+
+<main class="bg-white text-black">
+	<section class="max-w-6xl mx-auto py-12">
+		<h1 class="text-6xl font-bold mb-8 text-center tracking-widest">Worth a Watch</h1>
+		<p class="text-xl mb-12 text-center" />
+		<div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
+			<a href="#" class="bg-gray-700 hover:bg-gray-600 rounded-lg p-8 flex flex-col">
+				<span class="text-2xl font-bold mb-4 text-white">Movie 1</span>
+				<span class="text-lg text-white"
+					>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</span
+				>
+			</a>
+			<a href="#" class="bg-gray-700 hover:bg-gray-600 rounded-lg p-8 flex flex-col">
+				<span class="text-2xl font-bold mb-4 text-white">Movie 2</span>
+				<span class="text-lg text-white"
+					>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</span
+				>
+			</a>
+			<a href="#" class="bg-gray-700 hover:bg-gray-600 rounded-lg p-8 flex flex-col">
+				<span class="text-2xl font-bold mb-4 text-white">Movie 3</span>
+				<span class="text-lg text-white"
+					>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</span
+				>
+			</a>
+			<a href="#" class="bg-gray-700 hover:bg-gray-600 rounded-lg p-8 flex flex-col">
+				<span class="text-2xl font-bold mb-4 text-white">Movie 4</span>
+				<span class="text-lg text-white"
+					>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</span
+				>
+			</a>
+			<a href="#" class="bg-gray-700 hover:bg-gray-600 rounded-lg p-8 flex flex-col">
+				<span class="text-2xl font-bold mb-4 text-white">Movie 5</span>
+				<span class="text-lg text-white"
+					>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</span
+				>
+			</a>
+			<a href="#" class="bg-gray-700 hover:bg-gray-600 rounded-lg p-8 flex flex-col">
+				<span class="text-2xl font-bold mb-4 text-white">Movie 6</span>
+				<span class="text-lg text-white"
+					>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</span
+				>
+			</a>
+		</div>
+	</section>
+</main>
