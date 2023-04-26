0b0252f78a2dfc9578c22d96c6376399b723bf45
demioo <demiogun1@gmail.com>
2023-04-26 12:33:44 +0100

    improve fetching + styling

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,7 +1,36 @@
-import { contentfulClient } from '$lib/contentful-client';
+import contentfulFetch from '$lib/contentful-fetch';
+import { error } from '@sveltejs/kit';
+import type { BlogPost } from '../generated/graphql';
+
+const query = `
+{
+  blogPostCollection {
+    items {
+      title
+      slug
+      datePublished
+      featuredImage {
+        description
+        url
+      }
+    }
+  }
+}
+`;
 
 export async function load() {
+	const response = await contentfulFetch(query);
+
+	if (!response.ok) {
+		throw error(404, {
+			message: response.statusText
+		});
+	}
+
+	const { data } = await response.json();
+	const blogPosts: BlogPost[] = data.blogPostCollection.items;
+
 	return {
-		posts: contentfulClient.getEntries()
+		blogPosts
 	};
 }
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,4 @@
-<script>
+<script lang="ts">
 	import '../app.css';
 
 	export let data;
@@ -9,41 +9,28 @@
 		<h1 class="text-6xl font-bold mb-8 text-center tracking-widest">Worth a Watch</h1>
 		<p class="text-xl mb-12 text-center" />
 		<div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
-			<a href="#" class="bg-gray-700 hover:bg-gray-600 rounded-lg p-8 flex flex-col">
-				<span class="text-2xl font-bold mb-4 text-white">Movie 1</span>
-				<span class="text-lg text-white"
-					>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</span
+			{#each data?.blogPosts as post (post)}
+				<a
+					href={`/articles/${post.slug}`}
+					class="p-4 m-2 bg-white rounded-lg shadow-md hover:shadow-lg transition duration-300 ease-in-out sm:w-full"
 				>
+					<img src={post.featuredImage?.url} alt={post.featuredImage?.description} class="mb-2" />
+					<h3 class="text-gray-800 font-semibold text-lg leading-tight">{post.title}</h3>
+				</a>
+			{/each}
+			<a
+				href="#"
+				class="p-4 m-2 bg-white rounded-lg shadow-md hover:shadow-lg transition duration-300 ease-in-out sm:w-full"
+			>
+				<img src="https://via.placeholder.com/640x360" alt="Movie poster" class="mb-2" />
+				<h3 class="text-gray-800 font-semibold text-lg leading-tight">Movie Title</h3>
 			</a>
-			<a href="#" class="bg-gray-700 hover:bg-gray-600 rounded-lg p-8 flex flex-col">
-				<span class="text-2xl font-bold mb-4 text-white">Movie 2</span>
-				<span class="text-lg text-white"
-					>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</span
-				>
-			</a>
-			<a href="#" class="bg-gray-700 hover:bg-gray-600 rounded-lg p-8 flex flex-col">
-				<span class="text-2xl font-bold mb-4 text-white">Movie 3</span>
-				<span class="text-lg text-white"
-					>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</span
-				>
-			</a>
-			<a href="#" class="bg-gray-700 hover:bg-gray-600 rounded-lg p-8 flex flex-col">
-				<span class="text-2xl font-bold mb-4 text-white">Movie 4</span>
-				<span class="text-lg text-white"
-					>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</span
-				>
-			</a>
-			<a href="#" class="bg-gray-700 hover:bg-gray-600 rounded-lg p-8 flex flex-col">
-				<span class="text-2xl font-bold mb-4 text-white">Movie 5</span>
-				<span class="text-lg text-white"
-					>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</span
-				>
-			</a>
-			<a href="#" class="bg-gray-700 hover:bg-gray-600 rounded-lg p-8 flex flex-col">
-				<span class="text-2xl font-bold mb-4 text-white">Movie 6</span>
-				<span class="text-lg text-white"
-					>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</span
-				>
+			<a
+				href="#"
+				class="p-4 m-2 bg-white rounded-lg shadow-md hover:shadow-lg transition duration-300 ease-in-out sm:w-full"
+			>
+				<img src="https://via.placeholder.com/640x360" alt="Movie poster" class="mb-2" />
+				<h3 class="text-gray-800 font-semibold text-lg leading-tight">Movie Title</h3>
 			</a>
 		</div>
 	</section>
