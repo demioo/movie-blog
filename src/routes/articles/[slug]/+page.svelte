b01adb33d991c822ac93e18eb920b951cd7d60b0
demioo <demiogun1@gmail.com>
2023-05-22 22:33:52 +0100

    improve styling

--- src/routes/articles/[slug]/+page.svelte
+++ src/routes/articles/[slug]/+page.svelte
@@ -7,14 +7,16 @@
 
 <div class="container mx-auto">
 	<article class="max-w-4xl mx-auto my-8">
-		<h1 class="text-4xl font-bold text-gray-800 mb-4">Film Review: {article.title}</h1>
+		<h1 class="text-4xl font-bold text-gray-800 mb-4">{article.title}</h1>
 		<div class="flex flex-wrap items-center mb-4">
 			<img
 				src={article.featuredImage?.url}
 				alt={article.featuredImage?.description}
-				class="w-4/5 mb-4"
+				class="mb-4"
+				width="800"
+				height="450"
 			/>
-			<div class="text-lg leading-relaxed sm:w-2/3">
+			<div class="text-lg leading-relaxed sm:w-11/12">
 				{#if !article.content}
 					<p>No content</p>
 				{:else}
